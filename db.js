7c6baec3d5e8f30356fe69336eb5f7249d040747
Aaron Torres <51248787+aarontorres0@users.noreply.github.com>
2021-05-28 19:21:14 -0500

    added initial files

---
+++ db.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return {
+    replies: require('./replies.json'),
+    tweet: require('./tweet.json'),
+    retweeted_tweet: require('./retweeted_tweet.json'),
+    campaigns: require('./campaigns.json')
+  }
+}
